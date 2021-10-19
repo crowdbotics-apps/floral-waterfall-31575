952374dafa3befca250ef0231ad29921c9f913e9
Crowdbotics <team@crowdbotics.com>
2021-10-19 23:47:01 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "floral_waterfall_31575",
+  url: "https://floral_waterfall_31575.botics.co",
+  api: "https://floral_waterfall_31575.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
